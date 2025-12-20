a0baeacba7e7fcbb133426c577790e135baecb8e
Peter <75858369+1Pete3@users.noreply.github.com>
2025-12-20 11:43:21 -0500

    Delete Chapter07/7.10 — Sharing global constants across multiple files/constants.cpp

--- Chapter07/7.10 — Sharing global constants across multiple files/constants.cpp
+++
@@ -1,5 +0,0 @@
-namespace constants {
-    extern constexpr double pi{ 3.14159 };
-    extern constexpr double avogadro{ 6.0221413e23 };
-    extern constexpr double myGravity{ 9.2 }; // m/s^2 -- gravity is light on this planet
-}
