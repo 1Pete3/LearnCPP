97f80e2dd7fc32cadedf329b1efeed896e38a4dc
1pete3 <skowronekpeter103@gmail.com>
2026-02-03 17:42:07 -0500

    Added question #3

--- Chapter13/13.x — Chapter 13 summary and quiz/main.cpp
+++ Chapter13/13.x — Chapter 13 summary and quiz/main.cpp
@@ -36,11 +36,31 @@ void printMonster(const Monster& monster) {
     std::cout << "This " << getMonsterTypeString(monster.type) << " is named " << monster.name << " and has " << monster.health << " health." << std::endl;
 }
 
+//Question 2
+template <typename T>
+struct Triad {
+    T first{};
+    T second{};
+    T third{};
+};
+
+template<typename T>
+Triad(T, T, T) -> Triad<T>;
+
+template<typename T>
+void print(const Triad<T>& t) {
+    std::cout << '[' << t.first << ", " << t.second << ", " << t.third << ']';
+}
+
 int main()
 {
     Monster ogre{ Monster::ogre, "Shrek", 99999999 };
     Monster slime{ Monster::slime, "Blob", 10 };
     printMonster(ogre);
     printMonster(slime);
+    Triad t1{ 1,2,3 };
+    print(t1);
+    Triad t2{ 1.2,3.4,5.6 };
+    print(t2);
   return EXIT_SUCCESS;
 }
