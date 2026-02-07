7c694704a30535d2fbb2d18345399805d0d66c75
1pete3 <skowronekpeter103@gmail.com>
2026-02-07 11:00:46 -0500

    Added member functions returning references to data members

---
+++ Chapter14/14.7 — Member functions returning references to data members/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+
+class Employee {
+	std::string m_name{};
+
+public:
+	void setName(std::string_view name) { m_name = name; }
+	const std::string& getName() const { return m_name; }
+};
+
+int main()
+{
+	Employee joe{};
+	joe.setName("Joe");
+	std::cout << "Employee name is " << joe.getName() << std::endl;
+	return EXIT_SUCCESS;
+}
+
