3c7eda67fd7c49b859361bcd0c6a03e1d6498c17
1pete3 <skowronekpeter103@gmail.com>
2026-02-08 13:08:57 -0500

    Added encapsulation

---
+++ Chapter14/14.8 — The benefits of data hiding (encapsulation)/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+
+class Yogurt {
+    std::string m_flavor{ "vanilla" };
+
+public:
+    void setFlavor(std::string_view flavor) {
+        m_flavor = flavor;
+    }
+    const std::string& getFlavor() const {
+        return m_flavor;
+    }
+};
+
+void print(const Yogurt& y) {
+    std::cout << "The yogurt has the flavor " << y.getFlavor() << std::endl;
+}
+
+int main()
+{
+    Yogurt y{};
+    y.setFlavor("cherry");
+    print(y);
+
+    return EXIT_SUCCESS;
+}
