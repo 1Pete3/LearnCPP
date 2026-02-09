a20457e26a05bec6fd9c474877ff833960922e54
1pete3 <skowronekpeter103@gmail.com>
2026-02-09 17:51:37 -0500

    Added intro to constructors

---
+++ Chapter14/14.9 — Introduction to constructors/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+class Foo {
+private:
+    int m_x{};
+    int m_y{};
+public:
+    Foo(int x, int y) {
+        std::cout << "Foo(" << x << "," << y << ") constructed" << std::endl;
+    }
+
+    void print() const {
+        std::cout << "Foo(" << m_x << ", " << m_y << ")" << std::endl;
+    }
+};
+
+int main()
+{
+    Foo foo{ 6,7 };
+    foo.print();
+    return EXIT_SUCCESS;
+}
+
