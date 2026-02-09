ab30c8cb74c4984392ad18397bef91793dbe1aea
1pete3 <skowronekpeter103@gmail.com>
2026-02-09 18:17:15 -0500

    Added constructor member initializer lists

---
+++ Chapter14/14.10 — Constructor member initializer lists/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <string_view>
+class Ball {
+private:
+    std::string color{"none"};
+    double radius{0.0};
+public:
+    Ball(std::string_view color, double radius) :
+        color{ color },
+        radius{ radius } {
+    }
+    const std::string& getColor() const { return color; }
+    double getRadius() const { return radius; }
+};
+
+void print(const Ball& ball) {
+    std::cout << "Ball(" << ball.getColor() << ", " << ball.getRadius() << ")" << std::endl;
+}
+int main()
+{
+
+    Ball blue{ "blue", 10.0 };
+    print(blue);
+
+    Ball red{ "red", 12.0 };
+    print(red);
+
+    return EXIT_SUCCESS;
+}
