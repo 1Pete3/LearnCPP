db35b46d5364f475dadec9b5e65aab304a5e029b
1pete3 <skowronekpeter103@gmail.com>
2026-02-10 19:32:40 -0500

    Added default constructors and default arguments

---
+++ Chapter14/14.11 — Default constructors and default arguments/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class Foo {
+    private:
+        int m_x{};
+        int m_y{};
+
+    public:
+        Foo() {
+            std::cout << "Foo constructed" << std::endl;
+        }
+
+        Foo(int x = 0, int y = 0):
+        m_x{x},
+        m_y{y}
+        {
+            std::cout << "Foo(" << m_x << ", " << m_y << ") constructed" << std::endl;
+        }
+};
+
+int main()
+{
+    Foo foo1{};
+    Foo foo2{ 6,7 };
+    return EXIT_SUCCESS;
+}
+
