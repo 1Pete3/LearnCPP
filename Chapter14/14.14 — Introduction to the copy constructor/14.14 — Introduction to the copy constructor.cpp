50f5f25eed925373cff766f73c1a1a22f9087c87
1pete3 <skowronekpeter103@gmail.com>
2026-02-13 18:13:26 -0500

    Added copy constructor

---
+++ Chapter14/14.14 — Introduction to the copy constructor/14.14 — Introduction to the copy constructor.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+class Fraction {
+private:
+    int m_numerator{ 0 };
+    int m_denominator{ 1 };
+public:
+    Fraction(int numerator = 0, int denominator = 1) :
+        m_numerator{ numerator },
+        m_denominator{ denominator } {
+
+    }
+
+    Fraction(Fraction& fraction) :
+        m_numerator{ fraction.m_denominator },
+        m_denominator{ fraction.m_denominator } {
+        std::cout << "Copy constructor called" << std::endl;
+    }
+
+    void print() {
+        std::cout << "Fraction(" << m_numerator << ", " << m_denominator << ")" << std::endl;
+    }
+};
+int main()
+{
+    Fraction f{5,3};
+    Fraction fCopy{ f };
+    f.print();
+    fCopy.print();
+    return EXIT_SUCCESS;
+}
