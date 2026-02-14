20516cfac9c2716c37440e0457a24916a408c7b1
1pete3 <skowronekpeter103@gmail.com>
2026-02-14 17:30:56 -0500

    Added copy elision constructor

---
+++ Chapter14/14.15 — Class initialization and copy elision/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstdlib>
+class Something {
+public:
+    Something() = default;
+    Something(const Something&) {
+        std::cout << "Copy constructor called" << std::endl;
+    }
+};
+
+Something rvo() {
+    return Something{}; // Call something and copy constructor
+}
+
+
+Something nrvo() {
+    Something s{}; // Calls Something()
+    return s; // Calls copy contructor
+}
+
+int main()
+{
+    std::cout << "Initializing s1" << std::endl;
+    Something s1{ rvo() }; // Call copy constructor
+
+    std::cout << "Initializing s2" << std::endl;
+    Something s2{ nrvo()}; // Call copy constructor
+    return EXIT_SUCCESS;
+}
